65a4ff116b4a15d2dbce9fae30ee28068f44a278
Roope Hakulinen <roope.hakulinen@gmail.com>
2016-11-27 12:01:27 +0200

    Use -m to not need restart on the device.

--- push-code-production.sh
+++ push-code-production.sh
@@ -1,2 +1,2 @@
-code-push release-cordova activer-ios ios -d Production
-code-push release-cordova activer-android android -d Production
+code-push release-cordova activer-ios ios -d Production -m
+code-push release-cordova activer-android android -d Production -m
