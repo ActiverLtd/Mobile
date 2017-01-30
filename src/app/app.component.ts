74278b49f085b46c4e263debe3816fd9477eb56c
Roope Hakulinen <roope.hakulinen@gmail.com>
2017-01-30 18:09:12 +0200

    Wait for updates to be installed before proceeding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,43 +13,50 @@ export class MyApp {
 
   constructor(platform: Platform, translate: TranslateService, toastService: ToastService) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
+        // Okay, so the platform is ready and our plugins are available.
+        // Here you can do any higher level native things you might need.
+        StatusBar.styleDefault();
 
-      // this language will be used as a fallback when a translation isn't found in the current language
-      translate.setDefaultLang('fi');
+        // this language will be used as a fallback when a translation isn't found in the current language
+        translate.setDefaultLang('fi');
 
-      // the lang to use, if the lang isn't available, it will use the current loader to get them
-      translate.use('fi');
+        // the lang to use, if the lang isn't available, it will use the current loader to get them
+        translate.use('fi');
 
 
-      if (platform.is('cordova')) {
-        CodePush.sync().subscribe((syncStatus) => {
-          if (syncStatus === SyncStatus.UPDATE_INSTALLED) {
-            toastService.show('TOAST_UPDATES_INSTALLED');
-          }
-        });
-        window.setTimeout(() => {
-          Splashscreen.hide();
-        }, 300);
-        FCMPlugin.onNotification(
-          (data) => {
-            if (data.wasTapped) { // Notification was received on device tray and tapped by the user.
-              alert(JSON.stringify(data));
+        if (platform.is('cordova')) {
+          const hideSplashScreen = () => {
+            window.setTimeout(() => {
+              Splashscreen.hide();
+            }, 200);
+          };
+          CodePush.sync().subscribe((syncStatus) => {
+            if (syncStatus === SyncStatus.UPDATE_INSTALLED) {
+              toastService.show('TOAST_UPDATES_INSTALLED');
+              hideSplashScreen();
             }
-            else { // Notification was received in foreground. Maybe the user needs to be notified.
-              alert(JSON.stringify(data));
+            else if (syncStatus === SyncStatus.UP_TO_DATE || syncStatus === SyncStatus.UPDATE_IGNORED) {
+              hideSplashScreen();
             }
-          },
-          (msg) => {
-            console.log('onNotification callback successfully registered: ' + msg);
-          },
-          (err) => {
-            alert('Error registering onNotification callback: ' + err);
-          }
-        );
+          });
+          FCMPlugin.onNotification(
+            (data) => {
+              if (data.wasTapped) { // Notification was received on device tray and tapped by the user.
+                alert(JSON.stringify(data));
+              }
+              else { // Notification was received in foreground. Maybe the user needs to be notified.
+                alert(JSON.stringify(data));
+              }
+            },
+            (msg) => {
+              console.log('onNotification callback successfully registered: ' + msg);
+            },
+            (err) => {
+              alert('Error registering onNotification callback: ' + err);
+            }
+          );
+        }
       }
-    });
+    );
   }
 }
