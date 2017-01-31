8fcbeebbb64ac357ab3a29a7c70224cd48c3bec5
Roope Hakulinen <roope.hakulinen@gmail.com>
2017-02-01 00:34:03 +0200

    Don't close bash before acknowledged.

--- push-code-production.sh
+++ push-code-production.sh
@@ -1,2 +1,4 @@
 code-push release-cordova activer-ios ios -d Production -m
 code-push release-cordova activer-android android -d Production -m
+printf "\n\nPress any button to exit."
+read -n 1 key
--- push-code.sh
+++ push-code.sh
@@ -1,2 +1,4 @@
 code-push release-cordova activer-ios ios
 code-push release-cordova activer-android android
+echo "\n\nPress any button to exit."
+read -n 1 key
